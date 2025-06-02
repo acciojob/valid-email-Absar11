a8339720d28131f56c19230534f02be68fd7d291
Absar Ahmad <121477562+Absar11@users.noreply.github.com>
2025-06-02 09:47:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function validEmail(str) {
   //your JS code here.
+	const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  if (!str.trim()) return false;
+
+  // Check regex match 
+  return regex.test(str);
 }
 
 // Do not change the code below.
